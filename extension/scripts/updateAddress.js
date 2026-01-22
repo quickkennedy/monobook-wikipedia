269bb156900bf13f631388526dc4d460fc2b8f2d
Kennedy <38009297+quickkennedy@users.noreply.github.com>
2026-01-22 10:14:53 -0500

    Add files via upload

--- extension/scripts/updateAddress.js
+++ extension/scripts/updateAddress.js
@@ -3,8 +3,8 @@
  * https://github.com/torma616
  * 
  * 
- * This updates the URL on the webpage after the ?useskin=vector query has been applied,
- * to bring us back to the good old days when Vector was default, as it should be. 
+ * This updates the URL on the webpage after the ?useskin=monobook query has been applied,
+ * to bring us back to the good old days when monobook was default, as it should be. 
  * 
  **/ 
 'use strict';
@@ -12,7 +12,6 @@
 (async () => {
   const browser = chrome;
   let enabled = true;
-  let hideQuery = true;
 
   /**
    * Called when the settings are loaded from the local storage.
@@ -21,9 +20,6 @@
    */
   function onSettingsLoaded(items) {
     parseSettings(items);
-    if (enabled) {
-      checkURL(hideQuery);
-    }
   }
 
   /**
@@ -32,44 +28,7 @@
    * @param {[key: string]: any} items The items from the local storage.
    */
   function parseSettings(items) {
-    enabled = items.hasOwnProperty('vector_skin_activation') ? items.vector_skin_activation : true;
-    hideQuery = items.hasOwnProperty('hide_url_query') ? items.hide_url_query : true;
-  }
-
-  /**
-   * Processes the query string by removing the 'useskin=vector' parameter and updates the browser history.
-   * 
-   * @param {string} query The search string from the URL that needs to be processed.
-   */
-  function processQuery(query) {
-    let search = query.replace(/(\?|&)useskin=vector(&|$)/, '$1').replace(/&$/, '');
-    if (search === '?') {
-      search = '';
-    }
-    updateBrowserHistory(search)
-  }
-
-  /**
-   * Updates the browser history state with the new URL.
-   * 
-   * @param {string} search The updated search string part of the URL.
-   */
-  function updateBrowserHistory(search) {
-    const { origin, pathname, hash } = window.location;
-    window.history.replaceState(null, document.title, `${origin}${pathname}${search}${hash}`);
-  }
-
-  /**
-   * Checks the current URL's search string to determine if it contains the 'useskin=vector' parameter.
-   * If the parameter is present, it processes the query string to remove it.
-   * 
-   * @param {Boolean} isHidden Whether or not the query display is enabled via settings or not
-   */
-  function checkURL(isHidden) {
-    let { search } = window.location;
-    if (isHidden && search.includes('useskin=vector')) {
-      processQuery(search)
-    }
+    enabled = items.hasOwnProperty('monobook_skin_activation') ? items.monobook_skin_activation : true;
   }
 
   /**
