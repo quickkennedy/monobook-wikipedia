ad7eeb80affaed72e3bd637bda197c8930056d20
Kennedy <38009297+quickkennedy@users.noreply.github.com>
2026-01-22 10:15:41 -0500

    Add files via upload

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -3,27 +3,22 @@
 //for compatibility reasons
 var browser = chrome;
 
-var vectorSkinActivationSwitch = document.getElementById("vectorSkinActivationSwitch");
+var monobookSkinActivationSwitch = document.getElementById("monobookSkinActivationSwitch");
 var autoReloadSwitch = document.getElementById("autoReloadSwitch");
-var hideURLQuerySwitch = document.getElementById("hideURLQuerySwitch");
 
-var vectorSkinActivation = true;
+var monobookSkinActivation = true;
 var autoReload = true;
-var hideURLQuery = true;
 
 // load stored settings from local storage
 browser.storage.local.get((items) => {
-	vectorSkinActivation = items.hasOwnProperty('vector_skin_activation') ? items.vector_skin_activation : true;
+	monobookSkinActivation = items.hasOwnProperty('monobook_skin_activation') ? items.monobook_skin_activation : true;
 	autoReload = items.hasOwnProperty('auto_reload') ? items.auto_reload : true;
-	hideURLQuery = items.hasOwnProperty('hide_url_query') ? items.hide_url_query : true;
 
-	vectorSkinActivationSwitch.checked = vectorSkinActivation;
+	monobookSkinActivationSwitch.checked = monobookSkinActivation;
 	autoReloadSwitch.checked = autoReload;
-	hideURLQuerySwitch.checked = hideURLQuery;
 	
-	vectorSkinActivationSwitch.addEventListener("change", switchChanged);
+	monobookSkinActivationSwitch.addEventListener("change", switchChanged);
 	autoReloadSwitch.addEventListener("change", switchChanged);
-	hideURLQuerySwitch.addEventListener("change", switchChanged);
 });
 
 /**
@@ -31,13 +26,11 @@ browser.storage.local.get((items) => {
  */
 function switchChanged() {
 	// apply skin or not
-	vectorSkinActivation = vectorSkinActivationSwitch.checked;	
+	monobookSkinActivation = monobookSkinActivationSwitch.checked;	
 	autoReload = autoReloadSwitch.checked;
-	hideURLQuery = hideURLQuerySwitch.checked;
 
 	browser.storage.local.set({
-		vector_skin_activation: vectorSkinActivationSwitch.checked,
-		auto_reload: autoReloadSwitch.checked,
-		hide_url_query: hideURLQuerySwitch.checked
+		monobook_skin_activation: monobookSkinActivationSwitch.checked,
+		auto_reload: autoReloadSwitch.checked
 	});
 }
